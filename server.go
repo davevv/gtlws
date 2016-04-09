187bb38efd82a9518852d587ae2398cd53852037
David Villalobos <david.v.valdez@gmail.com>
2016-04-08 20:04:44 -0500

    a single handler for all urls

--- server.go
+++ server.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+type Handler struct {
+	cbk func(res http.ResponseWriter, req *http.Request)
+}
+
+func (han *Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	han.cbk(res, req)
+}
+
 func Start(cbk Page) {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+	http.ListenAndServe(":8080", &Handler{func(res http.ResponseWriter, req *http.Request) {
 		io.WriteString(res, cbk())
-	})
-	http.ListenAndServe(":8080", nil)
+	}})
 }
